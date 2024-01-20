b6ed3dd70ef7f2ac0d8e82a1314dc771406919fd
yash gupta <140940344+YashCommits@users.noreply.github.com>
2024-01-20 14:11:51 +0530

    Update FundMe.sol

--- Renix-FundMe/FundMe.sol
+++ Renix-FundMe/FundMe.sol
@@ -1,16 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {PriceConverter} from "./PriceConverter.sol";
+
+
 contract FundMe {
+
+    using PriceConverter for uint256; 
     
-    uint256 public myvalue = 1;
+    uint256 public minUSD = 5e18;
+
+    address[] public funders;
+    mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable {
-        myvalue = myvalue + 2;
-        require(msg.value > 1e18, "didn't sended enough Eth");
+        //myvalue = myvalue + 2;
+        require(msg.value.getConversionRate() >= minUSD, "didn't sended enough Eth");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
     // function withdraw(params) public {
     //     code
     // }
+
 }
