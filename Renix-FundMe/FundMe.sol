81adec72a1fd2ac5e001b1d5f7d5c22ff296e7d4
yash gupta <140940344+YashCommits@users.noreply.github.com>
2024-01-21 09:08:56 +0530

    Update FundMe.sol

--- Renix-FundMe/FundMe.sol
+++ Renix-FundMe/FundMe.sol
@@ -3,15 +3,22 @@ pragma solidity ^0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
 
 contract FundMe {
 
     using PriceConverter for uint256; 
     
-    uint256 public minUSD = 5e18;
+    uint256 public constant minUSD = 5e18;
 
     address[] public funders;
     mapping (address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner=msg.sender;
+    }
 
     function fund() public payable {
         //myvalue = myvalue + 2;
@@ -20,8 +27,37 @@ contract FundMe {
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
 
-    // function withdraw(params) public {
-    //     code
-    // }
+    function withdraw() public onlyOwner{
+        //require(msg.sender==owner, "must be owner!!!");
+        for(uint256 funderIndex=0; funderIndex<funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder]=0;
+        }
 
+        //reset the array
+        funders= new address[](0);
+
+        //actually withdraw the funds
+        //transfer
+        payable(msg.sender).transfer(address(this).balance);
+        //send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess,"send failed");
+        //call
+        (bool callSuccess, )= payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "call failed");
+    }
+
+    modifier onlyOwner(){
+        //require(msg.sender==i_owner,"Sender is not owner!");
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        _;
+    }
+
+    //what is someone send eth with fund function
+    //recieve & fallback special function
+    receive() external payable { fund(); }
+    fallback() external payable { fund(); }
 }
