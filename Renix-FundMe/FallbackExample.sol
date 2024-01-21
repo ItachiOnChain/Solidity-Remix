967799578b99f5413e86cc4a76427f031e27643c
yash gupta <140940344+YashCommits@users.noreply.github.com>
2024-01-21 09:10:50 +0530

    Create FallbackExample.sol

---
+++ Renix-FundMe/FallbackExample.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FallbackExample {
+    uint256 public result;
+    receive() external payable { result=1; }
+    fallback() external payable { result=2; }
+}
