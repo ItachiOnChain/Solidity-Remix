708db51b7d3efa1393baa4dda600fc704cddcd3e
UchihaWeb3 <guptayash1501@gmail.com>
2024-01-22 10:18:27 +0530

    fund me created

--- Renix-FundMe/FundMe.sol
+++ Renix-FundMe/FundMe.sol
@@ -17,12 +17,38 @@ contract FundMe {
     address public immutable i_owner;
 
     constructor() {
-        i_owner=msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
         //myvalue = myvalue + 2;
+
+        // Transaction Fields
+        // Nonce: tx count for the account
+        // GasPrice: price per unit of gas(wei)
+        // Gas Limit: max gas this tx can use
+        // To: address that the tx is sent to
+        // Value: amount of wei to send
+        // Data: what to send to the To address
+        // V, R, S: componenets of ECDSA signature values
+
+        // Transaction Value transfer
+        // Nonce: tx count for the account
+        // GasPrice: price per unit of gas(wei)
+        // Gas Limit: 21000
+        // To: address that the tx is sent to
+        // Value: amount of wei to send
+        // Data: empty
+        // V, R, S: componenets of ECDSA signature values
+
+        // if the require statement is false, then the function will stop executing and revert the transaction
         require(msg.value.getConversionRate() >= minUSD, "didn't sended enough Eth");
+
+        // Chainlink Price Feed: exchanges -> Data providers -> Chainlink Node -> Reference Smart Contract -> user Smart Contract
+        // Chainlink also provides "ChainLink VRF: Verifiable Random Function" for random number generation, because blockchain doesnot have randomness,
+        // so chainlink calculates the randomness and provides it to the smart contract
+        // Chainlink also provides "ChainLink Keepers / Chainlink Automation" for decentralized event driven execution
+
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
@@ -38,11 +64,11 @@ contract FundMe {
         funders= new address[](0);
 
         //actually withdraw the funds
-        //transfer
-        payable(msg.sender).transfer(address(this).balance);
-        //send
-        bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        require(sendSuccess,"send failed");
+        // //transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess,"send failed");
         //call
         (bool callSuccess, )= payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "call failed");
