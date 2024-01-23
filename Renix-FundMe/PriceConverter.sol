e4e354a0125f8c2a95bf1db0df2fe08a5a3829bf
UchihaWeb3 <guptayash1501@gmail.com>
2024-01-23 11:54:13 +0530

    chainlink price feed added

--- Renix-FundMe/PriceConverter.sol
+++ Renix-FundMe/PriceConverter.sol
@@ -1,34 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-interface AggregatorV3Interface {
-
-  function decimals() external view returns (uint8);
-  function description() external view returns (string memory);
-  function version() external view returns (uint256);
-
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-
-}
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter{
     function getPrice() internal view returns (uint256){
@@ -37,7 +10,7 @@ library PriceConverter{
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (,int256 price,,,) = priceFeed.latestRoundData();
         //price of eth in usd 2000.00000000
-        return uint256(price* 1e10);
+        return uint256(price* 1e10); // Typecasting int256 to uint256
     }
 
     function getConversionRate(uint256 ethAmount) internal  view returns (uint256){
