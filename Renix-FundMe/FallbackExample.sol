30ba5bc94ef9709619b08472bb910bfe3a1812fe
UchihaWeb3 <guptayash1501@gmail.com>
2024-02-01 01:24:37 +0530

    fallback and recive function done

--- Renix-FundMe/FallbackExample.sol
+++ Renix-FundMe/FallbackExample.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.7;
 
 contract FallbackExample {
     uint256 public result;
-    receive() external payable { result=1; }
-    fallback() external payable { result=2; }
+    receive() external payable {result = 1;}
+    fallback() external payable {result = 2;}
 }
